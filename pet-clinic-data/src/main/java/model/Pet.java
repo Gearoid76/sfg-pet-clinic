6b25475501ac571ffcadb820ef611dddd4043ab9
Gearoid <gocealla@gmail.com>
2019-11-07 14:39:12 +0100

    Adding this to the pet-clinic-data pom        <properties>             <spring-boot.repackage.skip>true</spring-boot.repackage.skip>        </properties>

--- pet-clinic-data/src/main/java/model/Pet.java
+++ pet-clinic-data/src/main/java/model/Pet.java
@@ -1,31 +1,35 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Pet {
     private PetType petType;
     private Owner owner;
-    private LocalDate birthdate;
+    private LocalDate birthDate;
 
     public PetType getPetType() {
+
         return petType;
     }
 
     public void setPetType(PetType petType) {
+
         this.petType = petType;
     }
 
     public Owner getOwner() {
+
         return owner;
     }
 
     public void setOwner(Owner owner) {
+
         this.owner = owner;
     }
 
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
+    public void setBirthDate(LocalDate birthdate) {
+        this.birthDate = birthDate;
 
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
+
     }
 }
